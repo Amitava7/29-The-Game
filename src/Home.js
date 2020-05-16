43980f551b071a937338531804006cbfd0365239
Amitava <amozumder@paypal.com>
2020-05-17 03:57:31 +0530

    proxy added

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,32 @@ function Home(props) {
   let click = ()=>{
     alert('sr');
   }
-  return (<div className="home">
-    <div class='title'>
-       <div class="pic"></div>
-      <div class="sign">
-        < span class="fast-flicker">29</span>... THE G<span class="flicker">A</span>ME
-      </div>
-    </div>
-    <div class="homebtns">
-      <div class="sign" onClick={click}>
-          Create Room
+  return (
+  <div className="home">
+        <div class='title'>
+          <div class="pic"></div>
+          <div class="sign" >
+            < span class="fast-flicker">29</span>... THE G<span class="flicker">A</span>ME
+          </div>
         </div>
-        <div class="sign" onClick={click}>
-          Join Room
-        </div>
-    </div>  
+        
+          {
+          true ?
+          <div class="loginbtns" onClick={()=>window.location="/api/test"}>
+            <div class="sign" >
+              Sign in with  < span class="fast-flicker"> Google</span>
+            </div> 
+            </div>
+            :
+          <div class="homebtns">
+            <div class="sign" onClick={click}>
+                Create Room
+              </div>
+              <div class="sign" onClick={click}>
+                Join Room
+              </div>
+              </div>    
+          }
   </div>);
 }
 
